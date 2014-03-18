59018c0438386736ffff449918957b21e9f9d43c
zhouyanming <zhouyanming@gmail.com>
2014-03-18 08:03:27 +0800

    x

--- webapp/assets/scripts/app.js
+++ webapp/assets/scripts/app.js
@@ -39,57 +39,60 @@ Observation.app = function(container) {
 			});
 
 	$('.trace-process', container).each(function() {
-		// var t = $(this).css('position', 'relative');
-		var t = $(this);
+		var t = $(this).css('position', 'relative');
 		var processInstanceId = t.data('processinstanceid');
 		t.html('<img alt="跟踪工作流" src="' + CONTEXT_PATH
 				+ '/process/processInstance/diagram/' + processInstanceId
 				+ '" style="position:absolute; left:0px; top:0px;">');
-		$.getJSON(CONTEXT_PATH + '/process/processInstance/trace/'
-						+ processInstanceId, function(data) {
-					$.each(data, function(i, v) {
-								var div = $('<div/>', {
-											'class' : 'activiyAttr'
-										}).css({
-											position : 'absolute',
-											left : (v.x - 1),
-											top : (v.y - 1),
-											width : (v.width - 2),
-											height : (v.height - 2)
-										}).appendTo(t).data('vars', v.vars);
-								if (v.currentActiviti) {
-									div.css({
-												'border' : '2px solid red',
-												'border-radius' : '12px'
-											});
+		var img = $('img', t)[0];
+		img.onload = function() {
+			var ratio = img.width / img.naturalWidth;
+			$.getJSON(CONTEXT_PATH + '/process/processInstance/trace/'
+							+ processInstanceId, function(data) {
+						$.each(data, function(i, v) {
+									var div = $('<div/>', {
+												'class' : 'activiyAttr'
+											}).css({
+												position : 'absolute',
+												left : (v.x * ratio - 1),
+												top : (v.y * ratio - 1),
+												width : (v.width * ratio - 2),
+												height : (v.height * ratio - 2)
+											}).appendTo(t).data('vars', v.vars);
+									if (v.currentActiviti) {
+										div.css({
+													'border' : '2px solid red',
+													'border-radius' : '12px'
+												});
+									}
+								});
+
+						if (typeof $.qtip != 'undefined') {
+							$('.activiyAttr').qtip({
+								content : function() {
+									var vars = $(this).data('vars');
+									var tipContent = '<table class="need-border">';
+									$.each(vars, function(varKey, varValue) {
+										if (varValue) {
+											tipContent += '<tr><td class="label">'
+													+ varKey
+													+ '</td><td>'
+													+ varValue + '<td/></tr>';
+										}
+									});
+									tipContent += '</table>';
+									return tipContent;
+								},
+								position : {
+									at : 'bottom left',
+									adjust : {
+										x : 3
+									}
 								}
 							});
+						}
+					});
+		}
 
-					if (typeof $.qtip != 'undefined') {
-						$('.activiyAttr').qtip({
-							content : function() {
-								var vars = $(this).data('vars');
-								var tipContent = '<table class="need-border">';
-								$.each(vars, function(varKey, varValue) {
-											if (varValue) {
-												tipContent += '<tr><td class="label">'
-														+ varKey
-														+ '</td><td>'
-														+ varValue
-														+ '<td/></tr>';
-											}
-										});
-								tipContent += '</table>';
-								return tipContent;
-							},
-							position : {
-								at : 'bottom left',
-								adjust : {
-									x : 3
-								}
-							}
-						});
-					}
-				});
 	});
 }
