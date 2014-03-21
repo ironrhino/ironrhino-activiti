e114327d3b7cad497fee953b9707829a17d6dc3e
zhouyanming <zhouyanming@gmail.com>
2014-03-21 12:25:18 +0800

    x

--- src/com/demo/service/UserService.java
+++ src/com/demo/service/UserService.java
@@ -3,10 +3,18 @@ package com.demo.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.ironrhino.security.model.User;
+import org.ironrhino.security.service.UserManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.demo.model.UserRole;
 
 @Component
 public class UserService {
+
+	@Autowired
+	private UserManager userManager;
 
 	public String findDeptLeader(String username) {
 		List<String> leaders = findDeptLeaders(username);
@@ -14,10 +22,11 @@ public class UserService {
 	}
 
 	public List<String> findDeptLeaders(String username) {
-		List<String> leaders = new ArrayList<String>();
-		//TODO
-		leaders.add("deptleader");
-		leaders.add("leaderuser");
+		List<User> users = userManager.findListByCriteria(userManager
+				.detachedCriteria(UserRole.deptLeader));
+		List<String> leaders = new ArrayList<String>(users.size());
+		for (User u : users)
+			leaders.add(u.getUsername());
 		return leaders;
 	}
 
