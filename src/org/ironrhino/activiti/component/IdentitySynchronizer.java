05186bb3d3fdda71b0f45679a79cdced71770719
zhouyanming <zhouyanming@gmail.com>
2015-08-03 11:05:26 +0800

    polishing

--- src/org/ironrhino/activiti/component/IdentitySynchronizer.java
+++ src/org/ironrhino/activiti/component/IdentitySynchronizer.java
@@ -3,7 +3,6 @@ package org.ironrhino.activiti.component;
 import org.activiti.engine.IdentityService;
 import org.activiti.engine.identity.Group;
 import org.ironrhino.core.event.EntityOperationEvent;
-import org.ironrhino.core.model.Persistable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.core.GrantedAuthority;
@@ -24,39 +23,33 @@ public class IdentitySynchronizer {
 	public void onApplicationEvent(EntityOperationEvent<? extends UserDetails> event) {
 		if (!event.isLocal())
 			return;
-		Persistable<?> entity = event.getEntity();
-		if (entity instanceof UserDetails) {
-			UserDetails user = (UserDetails) entity;
-			user = userDetailsService.loadUserByUsername(user.getUsername());
-			switch (event.getType()) {
-			case CREATE:
-			case UPDATE:
-				org.activiti.engine.identity.User u = identityService
-						.createUserQuery().userId(user.getUsername())
-						.singleResult();
-				if (u == null) {
-					u = identityService.newUser(user.getUsername());
-					identityService.saveUser(u);
-				}
-				for (Group group : identityService.createGroupQuery()
-						.groupMember(u.getId()).list())
-					identityService.deleteMembership(u.getId(), group.getId());
-				for (GrantedAuthority ga : user.getAuthorities()) {
-					Group g = identityService.createGroupQuery()
-							.groupId(ga.getAuthority()).singleResult();
-					if (g == null) {
-						g = identityService.newGroup(ga.getAuthority());
-						identityService.saveGroup(g);
-					}
-					identityService.createMembership(u.getId(), g.getId());
-				}
-				break;
-			case DELETE:
-				identityService.deleteUser(user.getUsername());
-				break;
-			default:
-				break;
+		UserDetails user = event.getEntity();
+		user = userDetailsService.loadUserByUsername(user.getUsername());
+		switch (event.getType()) {
+		case CREATE:
+		case UPDATE:
+			org.activiti.engine.identity.User u = identityService.createUserQuery().userId(user.getUsername())
+					.singleResult();
+			if (u == null) {
+				u = identityService.newUser(user.getUsername());
+				identityService.saveUser(u);
 			}
+			for (Group group : identityService.createGroupQuery().groupMember(u.getId()).list())
+				identityService.deleteMembership(u.getId(), group.getId());
+			for (GrantedAuthority ga : user.getAuthorities()) {
+				Group g = identityService.createGroupQuery().groupId(ga.getAuthority()).singleResult();
+				if (g == null) {
+					g = identityService.newGroup(ga.getAuthority());
+					identityService.saveGroup(g);
+				}
+				identityService.createMembership(u.getId(), g.getId());
+			}
+			break;
+		case DELETE:
+			identityService.deleteUser(user.getUsername());
+			break;
+		default:
+			break;
 		}
 	}
 
