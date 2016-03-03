3628149feea916458a03cd562f920f70ad5c4e3b
Yanming Zhou <zhouyanming@gmail.com>
2016-03-03 15:10:27 +0800

    Upgrade lib

--- src/org/ironrhino/activiti/component/HttpSessionFilterHookForIdentityService.java
+++ src/org/ironrhino/activiti/component/HttpSessionFilterHookForIdentityService.java
@@ -1,5 +1,8 @@
 package org.ironrhino.activiti.component;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.activiti.engine.IdentityService;
 import org.ironrhino.core.session.HttpSessionFilterHook;
 import org.ironrhino.core.util.AuthzUtils;
@@ -7,19 +10,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HttpSessionFilterHookForIdentityService implements
-		HttpSessionFilterHook {
+public class HttpSessionFilterHookForIdentityService implements HttpSessionFilterHook {
 
 	@Autowired
 	private IdentityService identityService;
 
 	@Override
-	public void beforeDoFilter() {
+	public boolean beforeFilterChain(HttpServletRequest request, HttpServletResponse response) {
 		identityService.setAuthenticatedUserId(AuthzUtils.getUsername());
+		return false;
 	}
 
 	@Override
-	public void afterDoFilter() {
+	public void afterFilterChain(HttpServletRequest request, HttpServletResponse response) {
 		identityService.setAuthenticatedUserId(null);
 	}
 
